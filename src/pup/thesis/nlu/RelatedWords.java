0e83539270424f949d563437745061bf0f750703
Paul <paulartigo@ymail.com>
2013-06-07 08:00:35 +0800

    added two new classes

---
+++ src/pup/thesis/nlu/RelatedWords.java
@@ -0,0 +1,17 @@
+package pup.thesis.nlu;
+
+import java.util.ArrayList;
+
+public class RelatedWords {
+	
+	private ArrayList<Word> words = new ArrayList<Word>();
+	
+	public void addWord() {
+		
+	}
+	
+	public void getRanking() {
+		
+	}
+	
+}
---
+++ src/pup/thesis/nlu/Word.java
@@ -0,0 +1,21 @@
+package pup.thesis.nlu;
+
+public class Word {
+	
+	public void setLabel() {
+		
+	}
+	
+	public void getLabel() {
+		
+	}
+	
+	public void getRank() {
+		
+	}
+	
+	public void setRank() {
+		
+	}
+	
+}
